a5a25d1171e376d51e5ab386986f47033eee88dc
Abdul-Basit Adebayo <abdulbazit@yahoo.ca>
2016-05-25 15:22:11 +0100

    populated blog page
    single blogs left.

--- application/views/blogs/blog.php
+++ application/views/blogs/blog.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('');
         <link href="<?=base_url()?>public/css/global.css" rel="stylesheet">
         <link href="<?=base_url()?>public/css/blogstyle.css" rel="stylesheet">
         <link href="<?=base_url()?>public/css/breadcrumb.css" rel="stylesheet">
+        <link href="<?=base_url()?>public/css/blogcontent.css" rel="stylesheet">
 
 <body>
             <!-- =========================================
@@ -65,3 +66,284 @@ defined('BASEPATH') OR exit('');
             </div><!-- /breadcrumb-section -->
 
     
+<!--#section-main-content-->
+<div class="content-section">    
+<section  class="dexp-section" id="section-main-content">
+	<div class="container">
+		<div class="row">
+			<!-- .region-content-->
+				<div class="region region-content col-xs-9 col-sm-9 col-md-9 col-lg-9">
+					<div class="tabs"></div>
+						<div id="block-system-main" class="block block-system">
+						  <div class="inner"></div>
+								
+							  <div  class="content">
+								<div class="view view-blog view-id-blog view-display-id-page_1 view-dom-id-a1631fe94e4713b2ee21bd11fa864d43">
+									
+							  
+								  <div class="view-content">
+								  
+								  
+									<div class="views-row views-row-1 views-row-odd views-row-first">
+										<article id="node-32" class="post teaser node node-article node-teaser view-mode-teaser clearfix"                                                     about="/orane/content/blog-fullwidth/post-title-1" typeof="sioc:Item foaf:Document">
+										<span property="dc:title" content="This is post Title - 1" class="rdf-meta element-hidden"></span><span                                               property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
+									  <!-- Begin media blog -->
+										<div class="media-blog">
+									
+										<div class="field-item even"><img typeof="foaf:Image" alt="" src="<?=base_url()?>public/img/blog/blog-1.png" width="780"                                            height="338" />        </div>
+
+										 <div class="row overlay-media" style="margin:0;">
+											<div class="col-xs-4 meta-col">
+											  <i class="fa fa-calendar-o fa-lg"></i>
+											  <span class="date">29 June </span>
+											</div>
+											<div class="col-xs-4 meta-col">
+											  <i class="fa fa-comment-o fa-lg"></i>
+											  <span class="comments"> 0 </span>
+											</div>
+											<div class="col-xs-4 meta-col">
+											   <span class="flag-wrapper flag-like flag-like-32">
+												<a href="blog-fullwidth.html?destination=blog-                            fullwidth&amp;token=b0b7232861a9e65a220052aa77a69ea6" title="Like this item" class="flag flag-action flag-link-toggle" rel="nofollow"><i class='fa fa-heart-o'></i></a><span class="flag-throbber">&nbsp;</span>
+												<span class="count-like">    
+													12</span>
+												</span>         
+											</div>
+										  </div>
+										</div> 
+									  <!-- End media blog -->
+										
+									  <section class="post-content">
+										<h1 class="post-title">
+										   <a href="content/blog-fullwidth/post-title-1.html">This is post Title - 1</a>
+										</h1>
+										<div class="author">
+										  Posted by:<span class="author-name"><span rel="sioc:has_creator"><span class="username" xml:lang="" about="/orane/users/admin" typeof="sioc:UserAccount" property="foaf:name" datatype="">admin</span></span></span> 
+												in
+										  <span class="author-name">
+										  <a href="blog-categories/ecommerce-solutions.html">Ecommerce Solutions</a>      </span>
+										</div>
+										<div class="summary">
+										  <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Massa turpis ornare nec commodo non, auctor vel felis. Etiam pharetra, turpis nec sollicitudin cursus vestibulum varius hendrerit turpis quiseam cursus. Nonec bibendum vestibulum Kravida. Alentesque condimentum orci vel leo laoreet accumsan interdum eros vel sapien tincidunt dignissim consequat. Aenean lacinia bibendum nulla sed consectetur.</p></div></div></div>    </div>
+										<div class="readmore">
+										  <a role="button" class="dexp-shortcodes-button btn btn-xs style02" href="content/blog-fullwidth/post-title-1.html">Read more</a>
+										</div>
+									  </section>
+									 </article>   
+									</div>
+							  <div class="views-row views-row-2 views-row-even">
+								<article id="node-31" class="post teaser node node-article node-teaser view-mode-teaser clearfix" about="/orane/content/blog/happy-friends-forever" typeof="sioc:Item foaf:Document">
+								<span property="dc:title" content="Happy Friends Forever" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
+							  <!-- Begin media blog -->
+								<div class="media-blog">
+									 
+									  <div id="dexp-carousel" class="carousel slide dexp_carousel field field-name-field-media field-type-media field-label-hidden" data-ride="carousel">
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner">
+										<div class="item field-item active"><img typeof="foaf:Image" alt="" src="<?=base_url()?>public/img/blog/blog-2.jpg" width="780" height="338" /></div>
+										<div class="item field-item "><img typeof="foaf:Image" alt="" src="<?=base_url()?>public/img/blog/blog-2.jpg" width="780" height="338" /></div>
+									  </div>
+
+								<!-- Controls -->
+								<a class="left carousel-control" href="#dexp-carousel" data-slide="prev">
+								  <span class="fa fa-angle-left"></span>
+								</a>
+								<a class="right carousel-control" href="#dexp-carousel" data-slide="next">
+								  <span class="fa fa-angle-right"></span>
+								</a>
+								</div>
+							  
+								  <div class="row overlay-media" style="margin:0;">
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-calendar-o fa-lg"></i>
+									  <span class="date">29 June </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-comment-o fa-lg"></i>
+									  <span class="comments"> 0 </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									   <span class="flag-wrapper flag-like flag-like-31">
+							<a href="blog-fullwidth.html?destination=blog-fullwidth&amp;token=bc9580da81284c2772afe716242086bc" title="Like this item" class="flag flag-action flag-link-toggle" rel="nofollow"><i class='fa fa-heart-o'></i></a><span class="flag-throbber">&nbsp;</span>
+							<span class="count-like">    
+								8</span>
+							</span>         </div>
+								  </div>
+								</div> 
+							  <!-- End media blog -->
+								
+							  <section class="post-content">
+								<h1 class="post-title">
+								   <a href="content/blog/happy-friends-forever.html">Happy Friends Forever</a>
+								</h1>
+								<div class="author">
+								  Posted by:<span class="author-name"><span rel="sioc:has_creator"><span class="username" xml:lang="" about="/orane/users/admin" typeof="sioc:UserAccount" property="foaf:name" datatype="">admin</span></span></span> 
+										in
+								  <span class="author-name">
+									<a href="blog-categories/drupal-development.html">Drupal Development</a>      </span>
+									  </div>
+								<div class="summary">
+								  <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Massa turpis ornare nec commodo non, auctor vel felis. Etiam pharetra, turpis nec sollicitudin cursus vestibulum varius hendrerit turpis quiseam cursus. Nonec bibendum vestibulum Kravida. Alentesque condimentum orci vel leo laoreet accumsan interdum eros vel sapien tincidunt dignissim consequat. Aenean lacinia bibendum nulla sed consectetur.</p></div></div></div>    </div>
+								<div class="readmore">
+								  <a role="button" class="dexp-shortcodes-button btn btn-xs style02" href="content/blog/happy-friends-forever.html">Read more</a>
+								</div>
+							  </section>
+							 </article>   </div>
+							  <div class="views-row views-row-3 views-row-odd">
+								<article id="node-18" class="post teaser node node-article node-teaser view-mode-teaser clearfix" about="/orane/content/blog/custom-blog-post-video" typeof="sioc:Item foaf:Document">
+								<span property="dc:title" content="Custom blog post with video" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
+							  <!-- Begin media blog -->
+								<div class="media-blog">
+									
+								
+									<div class="field-item even"><img typeof="foaf:Image" alt="Turning ideas into business: Where would you begin?" src="<?=base_url()?>public/img/blog/blog-3.jpg" />        </div>
+								
+								  
+
+								  <div class="row overlay-media" style="margin:0;">
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-calendar-o fa-lg"></i>
+									  <span class="date">06 May </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-comment-o fa-lg"></i>
+									  <span class="comments"> 0 </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									   <span class="flag-wrapper flag-like flag-like-18">
+							<a href="blog-fullwidth.html?destination=blog-fullwidth&amp;token=908a059105f3d2c4859a2c3480bd0b8d" title="Like this item" class="flag flag-action flag-link-toggle" rel="nofollow"><i class='fa fa-heart-o'></i></a><span class="flag-throbber">&nbsp;</span>
+							<span class="count-like">    
+								1</span>
+							</span>         </div>
+								  </div>
+								</div> 
+							  <!-- End media blog -->
+								
+							  <section class="post-content">
+								<h1 class="post-title">
+								   <a href="content/blog/custom-blog-post-video.html">Custom blog post with video</a>
+								</h1>
+								<div class="author">
+								  Posted by:<span class="author-name"><span rel="sioc:has_creator"><span class="username" xml:lang="" about="/orane/users/admin" typeof="sioc:UserAccount" property="foaf:name" datatype="">admin</span></span></span> 
+										in
+								  <span class="author-name">
+									<a href="taxonomy/term/3.html">Photography</a>      </span>
+									  </div>
+								<div class="summary">
+								  <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Massa turpis ornare nec commodo non, auctor vel felis. Etiam pharetra, turpis nec sollicitudin cursus vestibulum varius hendrerit turpis quiseam cursus. Nonec bibendum vestibulum Kravida. In vitae nunc tincidunt, euismod nibh sit amet, convallis arcu. Vestibulum feugiat auctor auctor. Phasellus lacinia auctor metus, in posuere justo egestas eget. Vivamus ornare tincidunt sagittis. Nunc pretium magna eu est condimentum malesuada. Nunc arcu nulla, fringilla in sodales sed, laoreet eget mi. Fusce ac suscipit turpis, sed porttitor mauris.</p></div></div></div>    </div>
+								<div class="readmore">
+								  <a role="button" class="dexp-shortcodes-button btn btn-xs style02" href="content/blog/custom-blog-post-video.html">Read more</a>
+								</div>
+							  </section>
+							 </article>   </div>
+							  <div class="views-row views-row-4 views-row-even">
+								<article id="node-16" class="post teaser node node-article node-teaser view-mode-teaser clearfix" about="/orane/content/blog/custom-blog-post-audio" typeof="sioc:Item foaf:Document">
+								<span property="dc:title" content="Custom blog post with audio" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="0" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
+							  <!-- Begin media blog -->
+								<div class="media-blog">
+									
+								
+									<div class="field-item even"><img typeof="foaf:Image" alt="" src="<?=base_url()?>public/img/blog/blog-4.jpg" width="780" height="338" />        </div>
+								
+								  
+
+								  <div class="row overlay-media" style="margin:0;">
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-calendar-o fa-lg"></i>
+									  <span class="date">06 May </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-comment-o fa-lg"></i>
+									  <span class="comments"> 0 </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									   <span class="flag-wrapper flag-like flag-like-16">
+							<a href="blog-fullwidth.html?destination=blog-fullwidth&amp;token=c1ed0a7b1c561355ddee13efaa8cf569" title="Like this item" class="flag flag-action flag-link-toggle" rel="nofollow"><i class='fa fa-heart-o'></i></a><span class="flag-throbber">&nbsp;</span>
+							<span class="count-like">    
+								1</span>
+							</span>         </div>
+								  </div>
+								</div> 
+							  <!-- End media blog -->
+								
+							  <section class="post-content">
+								<h1 class="post-title">
+								   <a href="content/blog/custom-blog-post-audio.html">Custom blog post with audio</a>
+								</h1>
+								<div class="author">
+								  Posted by:<span class="author-name"><span rel="sioc:has_creator"><span class="username" xml:lang="" about="/orane/users/admin" typeof="sioc:UserAccount" property="foaf:name" datatype="">admin</span></span></span> 
+										in
+								  <span class="author-name">
+									<a href="taxonomy/term/3.html">Photography</a>      </span>
+									  </div>
+								<div class="summary">
+								  <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet erat quis nibh vehicula, condimentum placerat lectus iaculis. Nam ultricies nisi vel ligula pulvinar, quis dapibus velit iaculis. In hac habitasse platea dictumst. In vitae nunc tincidunt, euismod nibh sit amet, convallis arcu. Vestibulum feugiat auctor auctor. Phasellus lacinia auctor metus, in posuere justo egestas eget. Vivamus ornare tincidunt sagittis. Nunc pretium magna eu est condimentum malesuada. Nunc arcu nulla, fringilla in sodales sed, laoreet eget mi. Fusce ac suscipit turpis, sed porttitor mauris.</p></div></div></div>    </div>
+								<div class="readmore">
+								  <a role="button" class="dexp-shortcodes-button btn btn-xs style02" href="content/blog/custom-blog-post-audio.html">Read more</a>
+								</div>
+							  </section>
+							 </article>   </div>
+							  <div class="views-row views-row-5 views-row-odd views-row-last">
+								<article id="node-15" class="post teaser node node-article node-teaser view-mode-teaser clearfix" about="/orane/content/blog/hop-hop-says-dog" typeof="sioc:Item foaf:Document">
+								<span property="dc:title" content="Hop Hop  says the Dog" class="rdf-meta element-hidden"></span><span property="sioc:num_replies" content="1" datatype="xsd:integer" class="rdf-meta element-hidden"></span>
+							  <!-- Begin media blog -->
+								<div class="media-blog">
+									
+								
+									<div class="field-item even"><img typeof="foaf:Image" alt="" src="<?=base_url()?>public/img/blog/blog-5.jpg" width="780" height="338" />        </div>
+								
+								  
+
+								  <div class="row overlay-media" style="margin:0;">
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-calendar-o fa-lg"></i>
+									  <span class="date">01 May </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									  <i class="fa fa-comment-o fa-lg"></i>
+									  <span class="comments"> 1 </span>
+									</div>
+									<div class="col-xs-4 meta-col">
+									   <span class="flag-wrapper flag-like flag-like-15">
+							<a href="blog-fullwidth.html?destination=blog-fullwidth&amp;token=804250af1379b250fbe156b1a5504a94" title="Like this item" class="flag flag-action flag-link-toggle" rel="nofollow"><i class='fa fa-heart-o'></i></a><span class="flag-throbber">&nbsp;</span>
+							<span class="count-like">    
+								5</span>
+							</span>         </div>
+								  </div>
+								</div> 
+							  <!-- End media blog -->
+								
+							  <section class="post-content">
+								<h1 class="post-title">
+								   <a href="content/blog/hop-hop-says-dog.html">Hop Hop  says the Dog</a>
+								</h1>
+								<div class="author">
+								  Posted by:<span class="author-name"><span rel="sioc:has_creator"><span class="username" xml:lang="" about="/orane/users/admin" typeof="sioc:UserAccount" property="foaf:name" datatype="">admin</span></span></span> 
+										in
+								  <span class="author-name">
+									<a href="taxonomy/term/3.html">Photography</a>      </span>
+									  </div>
+								<div class="summary">
+								  <div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus aliquet erat quis nibh vehicula, condimentum placerat lectus iaculis. Nam ultricies nisi vel ligula pulvinar, quis dapibus velit iaculis. In hac habitasse platea dictumst. In vitae nunc tincidunt, euismod nibh sit amet, convallis arcu. Vestibulum feugiat auctor auctor. Phasellus lacinia auctor metus, in posuere justo egestas eget. Vivamus ornare tincidunt sagittis. Nunc pretium magna eu est condimentum malesuada. Nunc arcu nulla, fringilla in sodales sed, laoreet eget mi. Fusce ac suscipit turpis, sed porttitor mauris.</p></div></div></div>    </div>
+								<div class="readmore">
+								  <a role="button" class="dexp-shortcodes-button btn btn-xs style02" href="content/blog/hop-hop-says-dog.html">Read more</a>
+								</div>
+							  </section>
+							 </article>   </div>
+								</div>
+							  
+							</div>  
+							  
+							  
+							</div> 
+						</div>
+					</div>
+				</div>
+				<!-- END .region-content-->
+		</div>
+	</div>
+</section>
+</div>    
+<!--END #section-main-content-->
+
+
+</body>
